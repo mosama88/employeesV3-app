4d04f7a6d3dec9bf328da3bc56198b4620011b09
Mohamed Osama <mosama88@hotmail.com>
2024-06-21 22:13:02 +0300

    roles

--- resources/views/role-permission/user/edit.blade.php
+++ resources/views/role-permission/user/edit.blade.php
@@ -1,5 +1,36 @@
-<x-app-layout>
+@extends('dashboard.layouts.master')
+@section('title', 'تعديل بيانات المستخدم')
+@section('css')
 
+    <!-- Internal Select2 css -->
+    <link href="{{URL::asset('dashboard/assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
+    <!--Internal  Datetimepicker-slider css -->
+    <link href="{{URL::asset('dashboard/assets/plugins/amazeui-datetimepicker/css/amazeui.datetimepicker.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/assets/plugins/jquery-simple-datetimepicker/jquery.simple-dtpicker.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/assets/plugins/pickerjs/picker.min.css')}}" rel="stylesheet">
+    <!-- Internal Spectrum-colorpicker css -->
+    <link href="{{URL::asset('dashboard/assets/plugins/spectrum-colorpicker/spectrum.css')}}" rel="stylesheet">
+    <!---Internal Fileupload css-->
+    <link href="{{URL::asset('dashboard/assets/plugins/fileuploads/css/fileupload.css')}}" rel="stylesheet" type="text/css"/>
+    <!---Internal Fancy uploader css-->
+    <link href="{{URL::asset('dashboard/assets/plugins/fancyuploder/fancy_fileupload.css')}}" rel="stylesheet" />
+    <!--Internal Sumoselect css-->
+    <link rel="stylesheet" href="{{URL::asset('dashboard/assets/plugins/sumoselect/sumoselect-rtl.css')}}">
+    <!--Internal  TelephoneInput css-->
+    <link rel="stylesheet" href="{{URL::asset('dashboard/assets/plugins/telephoneinput/telephoneinput-rtl.css')}}">
+@endsection
+@section('page-header')
+    <!-- breadcrumb -->
+    <div class="breadcrumb-header justify-content-between">
+        <div class="my-auto">
+            <div class="d-flex">
+                <h4 class="content-title mb-0 my-auto">المستخدمين</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ أضافة مستخدم</span>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb -->
+@endsection
+@section('content')
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
@@ -14,8 +45,8 @@
 
                 <div class="card">
                     <div class="card-header">
-                        <h4>Edit User
-                            <a href="{{ url('users') }}" class="btn btn-danger float-end">Back</a>
+                        <h4>تعديل بيانات المستخدم
+                            <a href="{{ url('users') }}" class="btn btn-danger float-end">رجوع</a>
                         </h4>
                     </div>
                     <div class="card-body">
@@ -24,23 +55,23 @@
                             @method('PUT')
 
                             <div class="mb-3">
-                                <label for="">Name</label>
+                                <label for="">الأسم</label>
                                 <input type="text" name="name" value="{{ $user->name }}" class="form-control" />
                                 @error('name') <span class="text-danger">{{ $message }}</span> @enderror
                             </div>
                             <div class="mb-3">
-                                <label for="">Email</label>
+                                <label for="">البريد الالكترونى</label>
                                 <input type="text" name="email" readonly value="{{ $user->email }}" class="form-control" />
                             </div>
                             <div class="mb-3">
-                                <label for="">Password</label>
+                                <label for="">كلمة المرور</label>
                                 <input type="text" name="password" class="form-control" />
                                 @error('password') <span class="text-danger">{{ $message }}</span> @enderror
                             </div>
                             <div class="mb-3">
-                                <label for="">Roles</label>
+                                <label for="">الصلاحيات</label>
                                 <select name="roles[]" class="form-control" multiple>
-                                    <option value="">Select Role</option>
+                                    <option value="">حدد الصلاحية</option>
                                     @foreach ($roles as $role)
                                         <option
                                             value="{{ $role }}"
@@ -53,7 +84,7 @@
                                 @error('roles') <span class="text-danger">{{ $message }}</span> @enderror
                             </div>
                             <div class="mb-3">
-                                <button type="submit" class="btn btn-primary">Update</button>
+                                <button type="submit" class="btn btn-primary">تحديث</button>
                             </div>
                         </form>
                     </div>
@@ -62,4 +93,46 @@
         </div>
     </div>
 
-</x-app-layout>
+
+@endsection
+
+@section('js')
+    <!--Internal  Datepicker js -->
+    <script src="{{URL::asset('dashboard/assets/plugins/jquery-ui/ui/widgets/datepicker.js')}}"></script>
+    <!--Internal  jquery.maskedinput js -->
+    <script src="{{URL::asset('dashboard/assets/plugins/jquery.maskedinput/jquery.maskedinput.js')}}"></script>
+    <!--Internal  spectrum-colorpicker js -->
+    <script src="{{URL::asset('dashboard/assets/plugins/spectrum-colorpicker/spectrum.js')}}"></script>
+    <!-- Internal Select2.min js -->
+    <script src="{{URL::asset('dashboard/assets/plugins/select2/js/select2.min.js')}}"></script>
+    <!--Internal Ion.rangeSlider.min js -->
+    <script src="{{URL::asset('dashboard/assets/plugins/ion-rangeslider/js/ion.rangeSlider.min.js')}}"></script>
+    <!--Internal  jquery-simple-datetimepicker js -->
+    <script src="{{URL::asset('dashboard/assets/plugins/amazeui-datetimepicker/js/amazeui.datetimepicker.min.js')}}"></script>
+    <!-- Ionicons js -->
+    <script src="{{URL::asset('dashboard/assets/plugins/jquery-simple-datetimepicker/jquery.simple-dtpicker.js')}}"></script>
+    <!--Internal  pickerjs js -->
+    <script src="{{URL::asset('dashboard/assets/plugins/pickerjs/picker.min.js')}}"></script>
+    <!-- Internal form-elements js -->
+    <script src="{{URL::asset('dashboard/assets/js/form-elements.js')}}"></script>
+
+    <!--Internal Fileuploads js-->
+    <script src="{{URL::asset('dashboard/assets/plugins/fileuploads/js/fileupload.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/fileuploads/js/file-upload.js')}}"></script>
+    <!--Internal Fancy uploader js-->
+    <script src="{{URL::asset('dashboard/assets/plugins/fancyuploder/jquery.ui.widget.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/fancyuploder/jquery.fileupload.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/fancyuploder/jquery.iframe-transport.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/fancyuploder/jquery.fancy-fileupload.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/fancyuploder/fancy-uploader.js')}}"></script>
+    <!--Internal  Form-elements js-->
+    <script src="{{URL::asset('dashboard/assets/js/advanced-form-elements.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/js/select2.js')}}"></script>
+    <!--Internal Sumoselect js-->
+    <script src="{{URL::asset('dashboard/assets/plugins/sumoselect/jquery.sumoselect.js')}}"></script>
+    <!-- Internal TelephoneInput js-->
+    <script src="{{URL::asset('dashboard/assets/plugins/telephoneinput/telephoneinput.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/telephoneinput/inttelephoneinput.js')}}"></script>
+
+    <script src="{{ asset('dashboard/assets/js/projects/add-users.js') }}"></script>
+@endsection
--- resources/views/role-permission/user/index.blade.php
+++ resources/views/role-permission/user/index.blade.php
@@ -1,9 +1,30 @@
-<x-app-layout>
-
+@extends('dashboard.layouts.master')
+@section('title', 'المستخدمين')
+@section('css')
+    <!-- Internal Data table css -->
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+@endsection
+@section('page-header')
+    <!-- breadcrumb -->
+    <div class="breadcrumb-header justify-content-between">
+        <div class="my-auto">
+            <div class="d-flex">
+                <h4 class="content-title mb-0 my-auto">المستخدمين</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ جدول المستخدمين</span>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb -->
+@endsection
+@section('content')
+    @include('dashboard.messages_alert')
     <div class="container mt-5">
-        <a href="{{ url('roles') }}" class="btn btn-primary mx-1">Roles</a>
-        <a href="{{ url('permissions') }}" class="btn btn-info mx-1">Permissions</a>
-        <a href="{{ url('users') }}" class="btn btn-warning mx-1">Users</a>
+        <a href="{{ url('roles') }}" class="btn btn-primary mx-1">الصلاحيات</a>
+        <a href="{{ url('permissions') }}" class="btn btn-info mx-1">الأذونات</a>
+        <a href="{{ url('users') }}" class="btn btn-warning mx-1">المستخدمين</a>
     </div>
 
     <div class="container mt-2">
@@ -13,12 +34,11 @@
                 @if (session('status'))
                     <div class="alert alert-success">{{ session('status') }}</div>
                 @endif
-
                 <div class="card mt-3">
                     <div class="card-header">
-                        <h4>Users
+                        <h4>المستخدمين
                             @can('create user')
-                                <a href="{{ url('users/create') }}" class="btn btn-primary float-end">Add User</a>
+                                <a href="{{ url('users/create') }}" class="btn btn-primary float-end">أضافة مستخدم</a>
                             @endcan
                         </h4>
                     </div>
@@ -27,11 +47,11 @@
                         <table class="table table-bordered table-striped">
                             <thead>
                             <tr>
-                                <th>Id</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Roles</th>
-                                <th>Action</th>
+                                <th>#</th>
+                                <th>أسم المستخدم</th>
+                                <th>البريد الالكترونى</th>
+                                <th>الصلاحيات</th>
+                                <th>العمليات</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -49,11 +69,11 @@
                                     </td>
                                     <td>
                                         @can('update user')
-                                            <a href="{{ url('users/'.$user->id.'/edit') }}" class="btn btn-success">Edit</a>
+                                            <a href="{{ url('users/'.$user->id.'/edit') }}" class="btn btn-success">تعديل</a>
                                         @endcan
 
                                         @can('delete user')
-                                            <a href="{{ url('users/'.$user->id.'/delete') }}" class="btn btn-danger mx-2">Delete</a>
+                                            <a href="{{ url('users/'.$user->id.'/delete') }}" class="btn btn-danger mx-2">حذف</a>
                                         @endcan
                                     </td>
                                 </tr>
@@ -67,4 +87,28 @@
         </div>
     </div>
 
-</x-app-layout>
+    <div class="main-navbar-backdrop"></div>
+
+@endsection
+
+@section('js')
+    <!-- Internal Data tables -->
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jszip.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+    <!--Internal  Datatable js -->
+    <script src="{{URL::asset('dashboard/assets/js/table-data.js')}}"></script>
+@endsection
