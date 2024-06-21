912bd824191d809696cbf1fecac0bb7f525df64d
Mohamed Osama <mosama88@hotmail.com>
2024-06-21 22:52:35 +0300

    roles225

--- resources/views/role-permission/role/index.blade.php
+++ resources/views/role-permission/role/index.blade.php
@@ -1,9 +1,30 @@
-<x-app-layout>
-
+@extends('dashboard.layouts.master')
+@section('title', 'الصلاحيات')
+@section('css')
+    <!-- Internal Data table css -->
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+@endsection
+@section('page-header')
+    <!-- breadcrumb -->
+    <div class="breadcrumb-header justify-content-between">
+        <div class="my-auto">
+            <div class="d-flex">
+                <h4 class="content-title mb-0 my-auto">الصلاحيات</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ جدول الصلاحيات</span>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb -->
+@endsection
+@section('content')
+    @include('dashboard.messages_alert')
 <div class="container mt-5">
-    <a href="{{ url('roles') }}" class="btn btn-primary mx-1">Roles</a>
-    <a href="{{ url('permissions') }}" class="btn btn-info mx-1">Permissions</a>
-    <a href="{{ url('users') }}" class="btn btn-warning mx-1">Users</a>
+    <a href="{{ url('roles') }}" class="btn btn-primary mx-1">الصلاحيات</a>
+    <a href="{{ url('permissions') }}" class="btn btn-info mx-1">الأذونات</a>
+    <a href="{{ url('users') }}" class="btn btn-warning mx-1">المستخدمين</a>
 </div>
 
 <div class="container mt-2">
@@ -17,20 +38,21 @@
             <div class="card mt-3">
                 <div class="card-header">
                     <h4>
-                        Roles
+                        الصلاحيات
                         @can('create role')
-                            <a href="{{ url('roles/create') }}" class="btn btn-primary float-end">Add Role</a>
+                            <a href="{{ url('roles/create') }}" class="btn btn-primary float-end">أضافة صلاحية</a>
                         @endcan
                     </h4>
                 </div>
                 <div class="card-body">
+                    <div class="table-responsive">
 
-                    <table class="table table-bordered table-striped">
+                        <table id="example" class="table key-buttons text-md-nowrap">
                         <thead>
                         <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th width="40%">Action</th>
+                            <th>#</th>
+                            <th>أسم الصلاحية</th>
+                            <th width="40%">العمليات</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -40,18 +62,18 @@
                                 <td>{{ $role->name }}</td>
                                 <td>
                                     <a href="{{ url('roles/'.$role->id.'/give-permissions') }}" class="btn btn-warning">
-                                        Add / Edit Role Permission
+                                        إضافة / تعديل أذونات الصلاحية
                                     </a>
 
                                     @can('update role')
                                         <a href="{{ url('roles/'.$role->id.'/edit') }}" class="btn btn-success">
-                                            Edit
+                                            تعديل
                                         </a>
                                     @endcan
 
                                     @can('delete role')
                                         <a href="{{ url('roles/'.$role->id.'/delete') }}" class="btn btn-danger mx-2">
-                                            Delete
+                                            حذف
                                         </a>
                                     @endcan
                                 </td>
@@ -65,4 +87,31 @@
         </div>
     </div>
 </div>
-</x-app-layout>
+    </div>
+    </div>   </div>
+
+    <div class="main-navbar-backdrop"></div>
+
+@endsection
+
+@section('js')
+    <!-- Internal Data tables -->
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jszip.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+    <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+    <!--Internal  Datatable js -->
+    <script src="{{URL::asset('dashboard/assets/js/table-data.js')}}"></script>
+@endsection
