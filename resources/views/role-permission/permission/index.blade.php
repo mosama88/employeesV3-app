ef490c7065ab6220c2b3394b822cf7e3dbbcf6d9
Mohamed Osama <mosama88@hotmail.com>
2024-06-21 22:26:59 +0300

    roles2

--- resources/views/role-permission/permission/index.blade.php
+++ resources/views/role-permission/permission/index.blade.php
@@ -1,9 +1,30 @@
-<x-app-layout>
-
+@extends('dashboard.layouts.master')
+@section('title', 'الأذونات')
+@section('css')
+    <!-- Internal Data table css -->
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('dashboard/assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+@endsection
+@section('page-header')
+    <!-- breadcrumb -->
+    <div class="breadcrumb-header justify-content-between">
+        <div class="my-auto">
+            <div class="d-flex">
+                <h4 class="content-title mb-0 my-auto">الأذونات</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ جدول الأذونات</span>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb -->
+@endsection
+@section('content')
+    @include('dashboard.messages_alert')
     <div class="container mt-5">
-        <a href="{{ url('roles') }}" class="btn btn-primary mx-1">Roles</a>
-        <a href="{{ url('permissions') }}" class="btn btn-info mx-1">Permissions</a>
-        <a href="{{ url('users') }}" class="btn btn-warning mx-1">Users</a>
+        <a href="{{ url('roles') }}" class="btn btn-primary mx-1">الصلاحيات</a>
+        <a href="{{ url('permissions') }}" class="btn btn-info mx-1">الأذونات</a>
+        <a href="{{ url('users') }}" class="btn btn-warning mx-1">المستخدمين</a>
     </div>
 
     <div class="container mt-2">
@@ -16,9 +37,9 @@
 
                 <div class="card mt-3">
                     <div class="card-header">
-                        <h4>Permissions
+                        <h4>الأذونات
                             @can('create permission')
-                                <a href="{{ url('permissions/create') }}" class="btn btn-primary float-end">Add Permission</a>
+                                <a href="{{ url('permissions/create') }}" class="btn btn-primary float-end">أضافة أذونات جديده</a>
                             @endcan
                         </h4>
                     </div>
@@ -27,9 +48,9 @@
                         <table class="table table-bordered table-striped">
                             <thead>
                             <tr>
-                                <th>Id</th>
-                                <th>Name</th>
-                                <th width="40%">Action</th>
+                                <th>#</th>
+                                <th>الأسم</th>
+                                <th width="40%">العمليات</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -39,11 +60,11 @@
                                     <td>{{ $permission->name }}</td>
                                     <td>
                                         @can('update permission')
-                                            <a href="{{ url('permissions/'.$permission->id.'/edit') }}" class="btn btn-success">Edit</a>
+                                            <a href="{{ url('permissions/'.$permission->id.'/edit') }}" class="btn btn-success">تعديل</a>
                                         @endcan
 
                                         @can('delete permission')
-                                            <a href="{{ url('permissions/'.$permission->id.'/delete') }}" class="btn btn-danger mx-2">Delete</a>
+                                            <a href="{{ url('permissions/'.$permission->id.'/delete') }}" class="btn btn-danger mx-2">حذف</a>
                                         @endcan
                                     </td>
                                 </tr>
@@ -57,4 +78,27 @@
         </div>
     </div>
 
-</x-app-layout>
+
+    @endsection
+
+    @section('js')
+        <!-- Internal Data tables -->
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/jszip.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+        <script src="{{URL::asset('dashboard/assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+        <!--Internal  Datatable js -->
+        <script src="{{URL::asset('dashboard/assets/js/table-data.js')}}"></script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PermissionController;
 use Carbon\Carbon;
 use App\Models\Employee;
 use App\Models\JobGrade;
@@ -93,9 +94,9 @@ Route::middleware(['auth', 'verified'])->name('dashboard.')->group(function () {
     // Our resource routes
     Route::resource('roles', RoleController::class);
     Route::resource('users', UserController::class);
+    Route::resource('/permissions', PermissionController::class);
 
 });
-
 
 //Route::group(['middleware' => ['role:super-admin|admin']], function() {
 //
